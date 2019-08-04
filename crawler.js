ee11c3fa2d57ccfde57c57182e6dac14f305c793
Faruk Durgut <frkdrgt@gmail.com>
2019-08-05 00:43:32 +0300

    index.js updated. setTimeout method tested. Crawler.js created.

---
+++ crawler.js
@@ -0,0 +1,147 @@
+#!/usr/bin/env node
+const { promisify } = require("util");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+const dom = require("./dom.json");
+
+const writeFileAsync = promisify(fs.writeFile);
+
+const CrawlerInstagram = class {
+  constructor() {}
+
+  async start(userName, postLimit) {
+    this.browser = await puppeteer.launch({
+      args: ["--lang=en-US", "--disk-cache-size=0"]
+    });
+
+    this.page = await this.browser.newPage();
+    await this.page.setExtraHTTPHeaders({
+      "Accept-Language": "en-US"
+    });
+    console.log(userName);
+    await this.page.goto(`https://instagram.com/${userName}`, {
+      waitUntil: "networkidle0"
+    });
+
+    if (await this.page.$(dom.notExist)) {
+      process.exit();
+    }
+
+    try {
+      this.dataProfile = {
+        urlProfile: this.page.url(),
+        ...(await this.getInfoProfile()),
+        posts: await this.getDataPostProfile(postLimit)
+      };
+      await this.writeProfile(userName);
+    } catch (error) {
+      console.error(error);
+      process.exit();
+    }
+    await this.browser.close();
+
+    return await this.dataProfile;
+  }
+
+  // Get info in profil
+  async getInfoProfile() {
+    return this.page.evaluate(dom => {
+      return {
+        numberPosts: document.querySelector(dom.numberPosts).innerText
+      };
+    }, dom);
+  }
+
+  // Get data for each post
+  async getDataPostProfile(postLimit) {
+    let numberPosts = await this.page.evaluate(dom => {
+      return document.querySelector(dom.numberPosts).innerText;
+    }, dom);
+
+    const convertedNumber = parseInt(numberPosts.split(",").join(""));
+    if (postLimit > convertedNumber) {
+      postLimit = convertedNumber;
+    }
+
+    const listPostUrl = new Set();
+
+    while (listPostUrl.size < Number(postLimit)) {
+      console.log(listPostUrl.size + " < " + postLimit);
+      const listUrl = await this.page.$$eval(dom.listPost, list =>
+        list.map(n => n.getAttribute("href"))
+      );
+
+      listUrl.forEach(url => {
+        if (!postLimit || (postLimit && listPostUrl.size < postLimit)) {
+          listPostUrl.add(url);
+        }
+      });
+
+      await this.page.evaluate(() => {
+        window.scrollTo(0, 1000000);
+      });
+    }
+
+    await this.page.close();
+
+    const listPost = [];
+
+    for (const url of listPostUrl) {
+      console.log(url);
+      const page = await this.browser.newPage();
+
+      await page.goto(`https://instagram.com${url}`, {
+        waitUntil: "networkidle0"
+      });
+      
+
+      const data = await page.evaluate(dom => {
+        return {
+          urlImage: document.querySelector(dom.urlImage)
+            ? document.querySelector(dom.urlImage).getAttribute("src")
+            : null,
+
+          video: document.querySelector(dom.video)
+            ? document.querySelector(dom.video).getAttribute("src")
+            : null,
+
+          description: document.querySelector(dom.description)
+            ? document.querySelector(dom.description).textContent
+            : null,
+
+          likeNumber: document.querySelector(dom.likeNumber)
+            ? document.querySelector(dom.likeNumber).textContent
+            : null,
+
+          videoViewsNumber: document.querySelector(dom.viewNumber)
+            ? document.querySelector(dom.viewNumber).textContent
+            : null
+        };
+      }, dom);
+
+      await page.close();
+      listPost.push(data);
+    }
+    return listPost;
+  }
+
+  async writeProfile(userName) {
+    let write = writeFileAsync(
+      `${userName}.json`,
+      JSON.stringify(this.dataProfile, null, 2)
+    );
+    return write.then(x => {
+      console.log("File created.");
+      
+    });
+    /*return writeFileAsync(
+			`${userName}.json`,
+			JSON.stringify(this.dataProfile, null, 2)
+		);*/
+  }
+};
+module.exports = CrawlerInstagram;
+// Simple usage
+////const crawler = new CrawlerInstagram();
+////crawler.start("evrimagaci",4).catch(error => console.error(error));
--- index.js
+++ index.js
@@ -1,133 +1,20 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const CrawlerInstagram = require("./crawler.js");
 
 const PORT = process.env.PORT || 5000;
+const app = express();
+app.get("/", async (req, res) => {
+    res.setHeader("Content-Type", "application/json");
 
-express()
-  .get("/user/:user/:count", (req, res) => {
-    (async () => {
-      const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox']});
-      const page = await browser.newPage();
+    const crawler = new CrawlerInstagram();
 
-      await page.setExtraHTTPHeaders({
-        "Accept-Language": "en-US"
-      });
-
-      let userName = req.params.user;
-      console.log(userName);
-
-      //Set username
-      await page.goto(`https://instagram.com/${userName}`, {
-        waitUntil: `networkidle2`
-      });
-
-      if (await page.$(`.dialog-404`)) {
-        process.exit();
-      }
-    
-      const textContent = await page.evaluate(() => document.querySelector('span.g47SY').textContent);
-      //const innerText = await page.evaluate(() => document.querySelector('span.g47SY').innerText);  Another way to get post count
-      
-      let postCount = parseInt(textContent.split(',').join(""));
-
-      let maxItemsSize = req.params.count;
-
-      let previousHeight;
-      var media = new Set();
-      let nodes = null;
-      var urlDetails = []; //will change
-      
-      await page.waitFor(1000);
-
-      if(postCount < maxItemsSize){
-        res.end("You can get "+ postCount +" pictures maximum");
-        browser.close();
-      }
-      else {
-        while (maxItemsSize == null || media.size < maxItemsSize) {
-          try 
-          {
-            
-            //Scroll page
-            previousHeight = await page.evaluate("document.body.scrollHeight");
-            await page.evaluate(`window.scrollTo(0, ${previousHeight})`);
-            //await page.waitForFunction(`document.body.scrollHeight  > ${previousHeight}`);
-            await page.waitFor(1000);
-
-
-            nodes = await page.evaluate(() => {
-              const images = document.querySelectorAll(
-                `a > div > div.KL4Bh > img`
-              );
-              return [].map.call(images, img => img.src);
-            });
-
-            //BEGIN TEST SECTION.
-            //
-            //
-            //This code will change after.
-
-            let urls = await page.evaluate(() => {
-              const images = document.querySelectorAll(
-                `div.v1Nh3 > a`
-              );
-              return [].map.call(images, img => img.href);
-            });
-
-
-            for(const imgUrl of urls){
-              console.log(imgUrl);
-              const page = await browser.newPage();
-              await page.setExtraHTTPHeaders({
-                "Accept-Language": "en-US"
-              });
-              await page.goto(imgUrl, {
-                waitUntil: 'networkidle0'
-              });
-
-              const data  = await page.evaluate(() => {
-                  return {
-                    post:{
-                      image: document.querySelector("div.KL4Bh img").getAttribute('src'),
-                      description: document.querySelector("div.C4VMK span").textContent,
-                      likeNumber : document.querySelector("div.Nm9Fw").textContent,
-                      multipleImage: !!document.querySelector("div.coreSpriteRightChevron")
-                    }
-                  }
-              });
-              
-              urlDetails.push(data);
-            }
-            //END TEST SECTION.
-  
-
-            nodes.forEach(element => {
-              if (media.size < maxItemsSize) {
-                media.add(element);
-              }
-            });
-          } catch (error) {
-            console.error(error);
-            break;
-          }
-        }
-
-        browser.close();
-        let items = media;
-  
-        var tmp = [];
-        items.forEach(url => {
-          tmp.push({
-            thumbnail_src: url,
-            accessibility_caption: "",
-            posts: urlDetails // for test
-          });
-        });
-        res.send(JSON.stringify(tmp));
-      }
-    })();
-  })
-  .get("/",(req,res) => {
-    res.end("Homepage");
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    const data = await crawler.start("#userName_parameter#", 50).catch(error => console.error(error));
+    res.json(data.posts);
+  });
+  const server = app.listen(PORT, '127.0.0.1', () => {
+    const { address, port } = server.address();
+    console.log("Example app listening at http://%s:%s", address, port);
+    server.setTimeout(500000);
+});
+  //.listen(PORT, () => console.log(`Listening on ${PORT}`));
